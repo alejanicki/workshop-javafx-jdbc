044641990c4ece98b136e7388690f0d83972dc8c
Alessandro Janicki Araujo <alessandrojanicki@hotmail.com>
2022-02-28 08:02:04 -0300

    Adding database access

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,31 +8,29 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-
 public class Program extends Application {
 
     private static Scene mainScene;
 
     @Override
-public void start(Stage primaryStage) {
-    try {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/ProgramView.fxml"));
-        ScrollPane scrollPane = loader.load();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/ProgramView.fxml"));
+            ScrollPane scrollPane = loader.load();
 
-        scrollPane.setFitToHeight(true);
-        scrollPane.setFitToWidth(true);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
 
-        mainScene = new Scene(scrollPane);
-        primaryStage.setScene(mainScene);
-        primaryStage.setTitle("Sample JavaFX application");
-        primaryStage.show();
-    } 
-    catch (IOException e) {
-    e.printStackTrace();
+            mainScene = new Scene(scrollPane);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
 
-    public static Scene getMainScene(){
+    public static Scene getMainScene() {
         return mainScene;
     }
 
